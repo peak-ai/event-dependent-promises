a8ac7e11de6e79f9ec036fa83424a68447fb514f
James Wright <james.wright@peak.ai>
2020-02-03 21:37:41 +0000

    Adds test case to detect subsequent event subscriptions

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,18 +2,24 @@ import eventDependent from './';
 import { EventEmitter } from 'events';
 
 describe('eventDependent', () => {
+  const methods = {
+    async getFoo(suffix: string) {
+      return `Foo${suffix}`;
+    },
+  };
+
+  let source: EventEmitter;
+  let augmented: typeof methods;
+
+  beforeEach(() => {
+    source = new EventEmitter();
+    augmented = eventDependent(source, 'ready', 'error', methods);
+  });
+
   it('should await an EventEmitter event before resolving the requested Promise', async () => {
-    const source = new EventEmitter();
-
-    const augmentedSource = eventDependent(source, 'ready', 'error', {
-      async getFoo(suffix: string) {
-        return `Foo${suffix}`;
-      },
-    });
-
     process.nextTick(() => source.emit('ready'));
 
-    const result = await augmentedSource.getFoo(' bar!');
+    const result = await augmented.getFoo(' bar!');
 
     expect(result).toBe('Foo bar!');
     expect(source.listenerCount('ready')).toBe(0);
@@ -21,19 +27,21 @@ describe('eventDependent', () => {
   });
 
   it('should reject if the fail event is emitted', async () => {
-    const source = new EventEmitter();
-
-    const augmentedSource = eventDependent(source, 'ready', 'error', {
-      async getFoo(suffix: string) {
-        return `Foo${suffix}`;
-      },
-    });
-
     process.nextTick(() => source.emit('error'));
 
-    await expect(augmentedSource.getFoo(' bar!')).rejects.toEqual(
+    await expect(augmented.getFoo(' bar!')).rejects.toEqual(
       new Error(`Event Dependent: Failure event error was fired`),
     );
+
+    expect(source.listenerCount('ready')).toBe(0);
+    expect(source.listenerCount('error')).toBe(0);
+  });
+
+  it('should not subscribe to the event source if a successful emission has already occured', async () => {
+    process.nextTick(() => source.emit('ready'));
+
+    await augmented.getFoo(' bar!');
+    await augmented.getFoo(' bar!');
 
     expect(source.listenerCount('ready')).toBe(0);
     expect(source.listenerCount('error')).toBe(0);
