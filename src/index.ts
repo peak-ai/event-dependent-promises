af832e184d663c39a0e7c6344a90c1081aa75e11
James Wright <james.wright@peak.ai>
2020-02-04 11:45:29 +0000

    Formats index

--- src/index.ts
+++ src/index.ts
@@ -12,26 +12,26 @@ const subscribe = (
   reject?: (e: Error) => void,
 ) => {
   /* We want to manually remove
-     * any remaining events to which
-     * we've subscribed to allow our
-     * source to be garbage collected. */
-    const onSuccess = () => {
-      eventSource.off(failureEvent, onFailure);
-      resolve();
-    };
+   * any remaining events to which
+   * we've subscribed to allow our
+   * source to be garbage collected. */
+  const onSuccess = () => {
+    eventSource.off(failureEvent, onFailure);
+    resolve();
+  };
 
-    const onFailure = () => {
-      eventSource.off(successEvent, onSuccess);
+  const onFailure = () => {
+    eventSource.off(successEvent, onSuccess);
 
-      if (reject) {
-        reject(
-          new Error(`Event Dependent: Failure event ${failureEvent} was fired`),
-        );
-      }
-    };
+    if (reject) {
+      reject(
+        new Error(`Event Dependent: Failure event ${failureEvent} was fired`),
+      );
+    }
+  };
 
-    eventSource.once(successEvent, onSuccess);
-    eventSource.once(failureEvent, onFailure);
+  eventSource.once(successEvent, onSuccess);
+  eventSource.once(failureEvent, onFailure);
 };
 
 const event = (
@@ -40,13 +40,7 @@ const event = (
   failureEvent: string,
 ) =>
   new Promise((resolve, reject) => {
-    subscribe(
-      eventSource,
-      successEvent,
-      failureEvent,
-      resolve,
-      reject,
-    );
+    subscribe(eventSource, successEvent, failureEvent, resolve, reject);
   });
 
 const eventDependentPromises = <
@@ -62,14 +56,9 @@ const eventDependentPromises = <
 
   /* Handles initial emission that
    * may occur during initialisaton */
-  subscribe(
-    eventSource,
-    successEvent,
-    failureEvent,
-    () => {
-      hasEmitted = true;
-    },
-  );
+  subscribe(eventSource, successEvent, failureEvent, () => {
+    hasEmitted = true;
+  });
 
   // Node 10 doesn't support Object.fromEntries :(
   return Object.entries(methods).reduce<Methods>((proxies, [name, func]) => {
